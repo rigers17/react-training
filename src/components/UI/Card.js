f928df723f33eff893565005c181cf46fc70a85a
rigers17 <qorajrigers@gmail.com>
2022-12-05 17:54:51 +0100

    commit 2

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -1,8 +1,8 @@
 import './Card.css';
 
-function Card(props) {
+function Card (props) {
 
-  const classes = "card" + props.children;
+  const classes = 'card ' + props.className;
 
   return <div className={classes}>{props.children}</div>;
 }
